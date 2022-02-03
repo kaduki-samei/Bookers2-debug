42a474c0199b9eecfd09eb50745ea7485980b0ff
kaduki-samei <kaduki0331@gmail.com>
2022-02-03 09:16:37 +0000

    [Up]searche_form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
-  get "home/about"=>"homes#about"
+  get "home/about" => "homes#about"
+  get "search" => "searches#search"
 
   resources :users, only: [:index,:show,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
