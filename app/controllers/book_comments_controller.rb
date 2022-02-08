01bdcc59a0b7cc58bd33ea4d96b58ebf9a9147c2
kaduki-samei <kaduki0331@gmail.com>
2022-02-08 09:44:25 +0000

    [Up]come_Ajax

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,20 +1,18 @@
 class BookCommentsController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
     comment = BookComment.new(book_comment_params)
-    comment.book_id = book.id
+    comment.book_id = @book.id
     comment.user_id = current_user.id
     comment.save
     # binding.pry
-    redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    book_comment = book.book_comments.find(params[:id])
+    @book = Book.find(params[:book_id])
+    book_comment = @book.book_comments.find(params[:id])
     book_comment.destroy
-    redirect_back(fallback_location: root_path)
   end
 
 
